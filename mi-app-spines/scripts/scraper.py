8e73af9b1bde94cdb7315570a03df97832c491e1
MartinEO05 <martinotero2005@hotmail.com>
2026-02-25 11:50:15 +0100

    commit 25/02

--- mi-app-spines/scripts/scraper.py
+++ mi-app-spines/scripts/scraper.py
@@ -27,7 +27,7 @@ DB_JSON_PATH = os.path.join(BASE_DIR, "public", "database.json")
 
 # Generamos términos: a-z, A-Z y franquicias clave (cumpliendo tu regla de variaciones)
 letras = "abcdefghijklmnopqrstuvwxyz"
-SEARCH_TERMS = ["Lewcifer820", "Mii203" ,"eridyon"] # + list(letras)
+SEARCH_TERMS = ["Lewcifer820", "Mii203" ,"eridyon","pand_ashh","Olivigarden", "TheKosmicKollector", "WarioPunk"] # + list(letras)
 
 # Límite alto para producción (básicamente sin límite)
 MAX_UPLOADS = 10000 
