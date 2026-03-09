e11b418bea97257f90c4ddcb095173b194830127
MartinEO05 <martinotero2005@hotmail.com>
2026-03-09 09:43:44 +0100

    commit

--- mi-app-spines/src/AboutView.jsx
+++ mi-app-spines/src/AboutView.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useMemo } from 'react';
-// Usamos ./ porque la carpeta 'data' está al mismo nivel que este archivo dentro de 'src'
 import { FUN_FACTS } from './data/funFactsData';
 // Componente para las cajas negras con borde blanco (Estilo Earthbound)
 const RPGBox = ({ children, style }) => (
--- mi-app-spines/src/data/funFactsData.js
+++ mi-app-spines/src/data/funFactsData.js
@@ -1,8 +1,4 @@
-// IMPORTANTE: En Vite/React, no puedes importar archivos de la carpeta /public 
-// directamente con 'import'. 
-// La mejor solución es mover el JSON a src/data/nombres_juegos.json 
-// O usar este formato si quieres mantenerlo ahí:
-
-import juegosData from '../../public/nombres_juegos.json';
+// Al estar en la misma carpeta, usamos ./
+import juegosData from './nombres_juegos.json';
 
 export const FUN_FACTS = juegosData;
